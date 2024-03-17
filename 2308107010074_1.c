1359c0af575fce3144934b33cb386fd7f77de032
nisakhrn <163709893+nisakhrn@users.noreply.github.com>
2024-03-17 13:34:24 +0700

    Update 2308107010074_1.c

--- 2308107010074_1.c
+++ 2308107010074_1.c
@@ -2,74 +2,66 @@
 
 int main() {
   int pilihan;
-  long int desimal, oktal, biner;
-  char str[100];
+  int desimal, biner, oktal;
 
-  // Menampilkan menu pilihan
   printf("Pilih jenis konversi:\n");
   printf("1. Desimal ke Biner\n");
   printf("2. Biner ke Desimal\n");
   printf("3. Desimal ke Oktal\n");
   printf("4. Oktal ke Desimal\n");
-  printf("Masukkan pilihan Anda: ");
+  printf("Masukkan pilihan: ");
   scanf("%d", &pilihan);
 
+  // Memproses pilihan dan melakukan konversi
   switch (pilihan) {
     case 1:
-      // Konversi Desimal ke Biner
       printf("Masukkan bilangan desimal: ");
-      scanf("%ld", &desimal);
+      scanf("%d", &desimal);
       biner = 0;
       int i = 1;
-      while (desimal != 0) {
+      while (desimal > 0) {
         biner += (desimal % 2) * i;
         desimal /= 2;
         i *= 10;
       }
-      printf("Bilangan binernya adalah: %ld\n", biner);
+      printf("Bilangan biner: %d\n", biner);
       break;
-
     case 2:
-      // Konversi Biner ke Desimal
       printf("Masukkan bilangan biner: ");
-      scanf("%s", str);
+      scanf("%d", &biner);
       desimal = 0;
-      int j = 0, len = strlen(str);
-      for (i = len - 1; i >= 0; i--) {
-        desimal += (str[i] - '0') * pow(2, j);
-        j++;
+      i = 1;
+      while (biner > 0) {
+        desimal += (biner % 10) * i;
+        biner /= 10;
+        i *= 2;
       }
-      printf("Bilangan desimalnya adalah: %ld\n", desimal);
+      printf("Bilangan desimal: %d\n", desimal);
       break;
-
     case 3:
-      // Konversi Desimal ke Oktal
       printf("Masukkan bilangan desimal: ");
-      scanf("%ld", &desimal);
+      scanf("%d", &desimal);
       oktal = 0;
       i = 1;
-      while (desimal != 0) {
+      while (desimal > 0) {
         oktal += (desimal % 8) * i;
         desimal /= 8;
         i *= 10;
       }
-      printf("Bilangan oktalnya adalah: %ld\n", oktal);
+      printf("Bilangan oktal: %d\n", oktal);
       break;
-
     case 4:
-      // Konversi Oktal ke Desimal
       printf("Masukkan bilangan oktal: ");
-      scanf("%s", str);
+      scanf("%d", &oktal);
       desimal = 0;
-      j = 0;
-      len = strlen(str);
-      for (i = len - 1; i >= 0; i--) {
-        desimal += (str[i] - '0') * pow(8, j);
-        j++;
+      i = 1;
+      while (oktal > 0) {
+        desimal += (oktal % 10) * i;
+        oktal /= 10;
+        i *= 8;
       }
-      printf("Bilangan desimalnya adalah: %ld\n", desimal);
+      printf("Bilangan desimal: %d\n", desimal);
       break;
-
     default:
       printf("Pilihan tidak valid!");
   }
